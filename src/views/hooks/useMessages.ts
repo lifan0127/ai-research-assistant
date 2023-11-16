aea9eaa14cbb57264f78a7bc10774e7764f8ed01
Fan Li <lifan0127@gmail.com>
2023-11-16 08:59:36 -0500

    fix: Message type error

--- src/views/hooks/useMessages.ts
+++ src/views/hooks/useMessages.ts
@@ -14,10 +14,10 @@ export function useMessages() {
 
   // Update a message in place without changing other messages in the list.
   // This should be use to only update message metadata that doesn't affect the conversation flow.
-  function editMessage(updatedMessage: Message) {
+  function editMessage(updatedMessage: Partial<Message>) {
     const messageIndex = messages.findIndex(message => message.id === updatedMessage.id)
     messages[messageIndex] = updatedMessage as Message
-    messageStore.modifyMessage(messageIndex, updatedMessage, false)
+    messageStore.modifyMessage(messageIndex, updatedMessage as Message, false)
   }
 
   // Update a message and trim all subsequent messages in the conversation
