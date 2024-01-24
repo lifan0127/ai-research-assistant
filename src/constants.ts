1f2cd9dc6ee3207a3cbefe18490318be80a524aa
Fan Li <lifan0127@gmail.com>
2024-01-24 09:13:52 -0500

    chore: add input character limit - suggestions only not enforced

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,4 @@ export const IS_ZOTERO_7 = Zotero.platformMajorVersion >= 102
 export const FEEDBACK_URL = 'https://zhy554vpjozvn75xcu7pz6fx2a0eeopz.lambda-url.us-east-1.on.aws'
 export const ARIA_LIBRARY = 'Aria AI Research Assistant'
 export const DEFAULT_BIB_STYLE = 'http://www.zotero.org/styles/american-chemical-society'
+export const INPUT_CHARACTER_LIMIT = 4000
