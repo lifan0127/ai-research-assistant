e50ef69b54c65742dc20658c385b9df39e35b501
lifan0127 <lifan0127@gmail.com>
2024-04-11 21:37:08 -0400

    fix: remove polyfills used by Zotero 6

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,6 @@
 import { BasicTool } from 'zotero-plugin-toolkit/dist/basic'
-import Addon from './addon'
 import { config } from '../package.json'
-import { IS_ZOTERO_7 } from './constants'
-import { fsPromises } from './polyfills/fs'
+// import { fsPromises } from './polyfills/fs'
 import './polyfills/object'
 import './polyfills/array'
 import './polyfills/string'
@@ -13,9 +11,6 @@ import { setTimeout, clearTimeout } from './polyfills/timers'
 import { ZoteroTextEncoder, ZoteroTextDecoder } from './polyfills/textencoder'
 
 export default function globalConfig() {
-  if (!IS_ZOTERO_7) {
-    Components.utils.importGlobalProperties(['URL', 'URLSearchParams', 'fetch'])
-  }
 
   // process.env = { ...process.env, LANGCHAIN_TRACING: __env__ === 'development' ? 'true' : 'false' }
   if (__env__ === 'development') {
@@ -33,28 +28,56 @@ export default function globalConfig() {
 
   const basicTool = new BasicTool()
 
+  function defineGlobal(name: Parameters<BasicTool["getGlobal"]>[0]): void;
+  function defineGlobal(name: string, getter: () => any): void;
+  function defineGlobal(name: string, getter?: () => any) {
+    Object.defineProperty(_globalThis, name, {
+      get() {
+        return getter ? getter() : basicTool.getGlobal(name);
+      },
+    });
+  }
+
   if (!basicTool.getGlobal('Zotero')[config.addonInstance]) {
     // Set global variables
-    _globalThis.Zotero = basicTool.getGlobal('Zotero')
-    _globalThis.ZoteroPane = basicTool.getGlobal('ZoteroPane')
-    _globalThis.Zotero_Tabs = basicTool.getGlobal('Zotero_Tabs')
-    _globalThis.window = basicTool.getGlobal('window')
-    _globalThis.document = basicTool.getGlobal('document')
-    _globalThis.setTimeout = setTimeout
-    _globalThis.clearTimeout = clearTimeout
-    _globalThis.fs = fsPromises
-    _globalThis.crypto = crypto
-    _globalThis.TextEncoder = ZoteroTextEncoder
-    _globalThis.TextDecoder = ZoteroTextDecoder
-    _globalThis.addon = new Addon()
-    _globalThis.ztoolkit = addon.data.ztoolkit as any
-    ztoolkit.basicOptions.log.prefix = `[${config.addonName}]`
-    ztoolkit.basicOptions.log.disableConsole = addon.data.env === 'production'
-    ztoolkit.UI.basicOptions.ui.enableElementJSONLog = addon.data.env === 'development'
-    ztoolkit.UI.basicOptions.ui.enableElementDOMLog = addon.data.env === 'development'
-    ztoolkit.basicOptions.debug.disableDebugBridgePassword = addon.data.env === 'development'
-    Zotero[config.addonInstance] = addon
-    // Trigger addon hook for initialization
-    addon.hooks.onStartup()
+    // _globalThis.Zotero = basicTool.getGlobal('Zotero')
+    // _globalThis.console = basicTool.getGlobal('Zotero').getMainWindow().console
+    // _globalThis.console = basicTool.getGlobal('window').console
+    // _globalThis.ZoteroPane = basicTool.getGlobal('ZoteroPane')
+    // _globalThis.Zotero_Tabs = basicTool.getGlobal('Zotero_Tabs')
+    // _globalThis.window = basicTool.getGlobal('window')
+    // _globalThis.document = basicTool.getGlobal('document')
+    // _globalThis.setTimeout = setTimeout
+    // _globalThis.clearTimeout = clearTimeout
+    // // _globalThis.fs = fsPromises
+    // _globalThis.crypto = crypto
+    // _globalThis.TextEncoder = ZoteroTextEncoder
+    // _globalThis.TextDecoder = ZoteroTextDecoder
+    // _globalThis.addon = new Addon()
+    // _globalThis.ztoolkit = addon.data.ztoolkit as any
+    // ztoolkit.basicOptions.log.prefix = `[${config.addonName}]`
+    // ztoolkit.basicOptions.log.disableConsole = addon.data.env === 'production'
+    // ztoolkit.UI.basicOptions.ui.enableElementJSONLog = addon.data.env === 'development'
+    // ztoolkit.UI.basicOptions.ui.enableElementDOMLog = addon.data.env === 'development'
+    // ztoolkit.basicOptions.debug.disableDebugBridgePassword = addon.data.env === 'development'
+    // Zotero[config.addonInstance] = addon
+    // // Trigger addon hook for initialization
+    // addon.hooks.onStartup()
+
+    defineGlobal("console", () => {
+      const window = basicTool.getGlobal('Zotero').getMainWindow() as any
+      return window.console
+    });
+    defineGlobal("window");
+    defineGlobal("document");
+    defineGlobal("ZoteroPane");
+    defineGlobal("Zotero_Tabs");
+
+    const Addon = require('./addon').default
+    _globalThis.addon = new Addon();
+    defineGlobal("ztoolkit", () => {
+      return _globalThis.addon.data.ztoolkit;
+    });
+    Zotero[config.addonInstance] = addon;
   }
 }
