be6dea08f999b975f8cd786621dd50f8fdeaa336
Fan Li <lifan0127@gmail.com>
2025-01-01 18:05:04 -0500

    fix: remove legacy polyfills

--- src/legacyPolyfills/array.ts
+++
@@ -1,7 +0,0 @@
-if (!Array.prototype.flatMap) {
-  Array.prototype.flatMap = function (callback, thisArg) {
-    return this.reduce(function (acc, item, index, array) {
-      return acc.concat(callback.call(thisArg as any, item, index, array))
-    }, [])
-  }
-}
--- src/legacyPolyfills/textencoder.ts
+++
@@ -1,6 +0,0 @@
-const { TextEncoder: ZoteroTextEncoder, TextDecoder: ZoteroTextDecoder } = Components.utils.import(
-  'resource://gre/modules/Services.jsm',
-  {}
-)
-
-export { ZoteroTextEncoder, ZoteroTextDecoder }
